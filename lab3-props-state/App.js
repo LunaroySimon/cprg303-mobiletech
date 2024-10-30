b14c4cd2c2ca3c9fba6e63f4c1b29241965606c7
Lunaroy13 <romano_luna@hotmail.com>
2024-10-30 16:34:52 -0600

    lab3 completion

---
+++ lab3-props-state/App.js
@@ -0,0 +1,24 @@
+/**
+ * My To Do List App
+ *
+ * @format
+ */
+
+import { SafeAreaView } from 'react-native';
+import ToDoList from './components/ToDoList';
+import ToDoForm from './components/ToDoForm';
+import { useState } from 'react';
+
+
+function App() {
+  const [tasks, setTasks] = useState([ 'Do laundry', 'Go to gym', 'Walk dog' ]);
+
+  return (
+    <SafeAreaView>
+      <ToDoList tasks={tasks}/>
+      <ToDoForm />
+    </SafeAreaView>
+  );
+}
+
+export default App;
